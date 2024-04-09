1113a44ebf5de10f9f19462b86bc4a9dc437ab59
UchuVoid <2815762528@qq.com>
2024-04-09 22:30:21 +0800

    I come back again!>

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,29 +3,43 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-
-    private Comparator<T> c;
+    private final Comparator<T> comparator;
 
     public MaxArrayDeque(Comparator<T> c) {
-        this.c = c;
+        comparator = c;
     }
 
-    public T max() {
-        return max(c);
-    }
-
-    public T max(Comparator<T> comparator) {
+    public T max(Comparator<T> c) {
         if (isEmpty()) {
             return null;
         }
-
-        T maxValue = get(0);
-        for (int i = 0; i < size(); i++) {
-            if (comparator.compare(get(i), maxValue) > 0) {
-                maxValue = get(i);
+        int maxIndex = 0;
+        for (int i = 1; i < size(); i++) {
+            if (c.compare(get(i), get(maxIndex)) > 0) {
+                maxIndex = i;
             }
         }
+        return get(maxIndex);
+    }
 
-        return maxValue;
+    public T max() {
+        return max(comparator);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof MaxArrayDeque)) {
+            return false;
+        }
+        if (((MaxArrayDeque<?>) o).max() != max()) {
+            return false;
+        }
+        return super.equals(o);
     }
 }
--- proj1/gh2/GuitarHero.java
+++ proj1/gh2/GuitarHero.java
@@ -6,12 +6,12 @@ import edu.princeton.cs.algs4.StdDraw;
  * A client that uses the synthesizer package to replicate a plucked guitar string sound
  */
 public class GuitarHero {
-    private String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
     public static final double CONCERT_A = 440.0;
     public static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
-//    private double calculFrequency(int i){
-//        return
-//    }
+    private double calculFrequency(int i){
+        return 440*Math.pow(2,(int)((i-24)/12));
+    }
     public static void main(String[] args) {
         /* create two guitar strings, for concert A and C */
         GuitarString stringA = new GuitarString(CONCERT_A);
